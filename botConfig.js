cbf6ef4809319ac8279009d772ec208ab7770dcd
skatharv <86115034+skatharv@users.noreply.github.com>
2022-10-16 20:57:33 +0530

    Add files via upload

---
+++ botConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "OTI4MzAxMDYzMDMzOTc4OTAw.G2t8Sm.2u7jPXiCuS-H2V0WtatX4JWZEm6a3TmvFZEUc0",
+    prefix: "sk!",
+    admins: [
+        "836263073915732020"
+],
+    debug: true,
+    countChannel: "1002433763122352128"
+};
